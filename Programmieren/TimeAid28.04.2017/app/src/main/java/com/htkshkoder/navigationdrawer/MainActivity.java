8933e717d68205c157294fbaa060997a0e013c7f
bjornak3 <bjornak3@gmail.com>
2017-05-05 14:25:07 +0200

    neue Version KlassenLogik
    KlassenLogik implementiert

--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/MainActivity.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                setContentView(R.layout.eintrag_layout);
             }
         });
 
--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/eintrag.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/eintrag.java
@@ -2,11 +2,13 @@ package com.htkshkoder.navigationdrawer;
 
 import android.app.Activity;
 
+import android.support.v7.app.AppCompatActivity;
+
+
 /**
  * Created by bjorn on 05.05.2017.
  */
 
-public class eintrag {
+public class Eintrag extends AppCompatActivity {
 
-    String date,name;
 }
