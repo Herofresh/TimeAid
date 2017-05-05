0eb7cb7b9e4e0a192603814a702e2c48e2333bc4
bjornak3 <bjornak3@gmail.com>
2017-05-05 14:58:24 +0200

    fuck it

--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/eintrag.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/eintrag.java
@@ -1,14 +1,8 @@
 package com.htkshkoder.navigationdrawer;
 
-import android.app.Activity;
-
-import android.support.v7.app.AppCompatActivity;
-
-
 /**
  * Created by bjorn on 05.05.2017.
  */
 
-public class Eintrag extends AppCompatActivity {
-
+public class eintrag {
 }
