1c3c9e68f2ced95f8949e4b372cdebf5b6df35da
bjornak3 <bjornak3@gmail.com>
2017-05-05 12:20:19 +0200

    CalenderView_verbessert
    CalenderView stürzt nicht mehr ab........yaaaaaaay

--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/CalendarFragment.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/CalendarFragment.java
@@ -4,11 +4,13 @@ package com.htkshkoder.navigationdrawer;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 /**
  * Created by FeJo on 28.04.2017.
@@ -23,23 +25,24 @@ public class CalendarFragment extends Fragment {
     public CalendarView onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
 
-            myView = (CalendarView) inflater.inflate(R.layout.calendarlo, container, false);
+        myView = (CalendarView) inflater.inflate(R.layout.calendarlo, container, false);
 
-        return myView;
 
-         //myView = (CalendarView)inflater.inflate(R.layout.calendarlo, container, false);
+
+        //myView = (CalendarView)inflater.inflate(R.layout.calendarlo, container, false);
 
         //myView =  (CalendarView)getView().findViewById(R.id.simpleCalendarView);
 
 
-//        simpleCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-//                month +=1;
-//                Toast.makeText(getActivity(),dayOfMonth + "/" + month +"/" + year, Toast.LENGTH_LONG).show();
-//            }
-//        });
-        //return myView;
+        myView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+                month +=1;
+                Toast.makeText(getActivity(),dayOfMonth + "/" + month +"/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+        return myView;
         //return super.onCreateView(inflater, container, savedInstanceState);
 
 
