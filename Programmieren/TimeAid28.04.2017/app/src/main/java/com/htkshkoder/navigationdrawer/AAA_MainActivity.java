b34e5e6c679396fb834d1a9e253d3af7f66d6aad
bjornak3 <bjornak3@gmail.com>
2017-05-12 16:20:23 +0200

    irgendeinVersion

--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/AAA_MainActivity.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/AAA_MainActivity.java
@@ -1,5 +1,6 @@
 package com.htkshkoder.navigationdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -30,7 +31,9 @@ public class AAA_MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                setContentView(R.layout.calendar_a_eintrag_lo);
+                //setContentView(R.layout.calendar_a_eintrag_lo);
+                Intent in  = new Intent(AAA_MainActivity.this,Calendar_A_Eintrag.class);
+                startActivity(in);
             }
         });
 
--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/Calendar_A_Eintrag.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/Calendar_A_Eintrag.java
@@ -1,14 +1,42 @@
 package com.htkshkoder.navigationdrawer;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 /**
  * Created by bjorn on 05.05.2017.
  */
 
-public class Calendar_A_Eintrag {
+public class Calendar_A_Eintrag extends Activity{
     /*
         Variablen:
             datum,name,description,priority,kategorie
         Methoden:
             showEintrag()
      */
+    Button btn;
+    EditText et;
+    TextView tv;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_a_eintrag_lo);
+
+        btn = (Button) findViewById(R.id.btnSubmit);
+        et = (EditText)findViewById(R.id.eTName);
+        tv = (TextView)findViewById(R.id.tVeintrag);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(et.getText());
+            }
+        });
+    }
 }
