ae08ecce524ee02c895e221df5f2c38742cc5b3b
bjornak3 <bjornak3@gmail.com>
2017-05-26 20:10:28 +0200

    GUI Logo integriert v1

--- Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/AAA_MainActivity.java
+++ Programmieren/TimeAid28.04.2017/app/src/main/java/com/htkshkoder/navigationdrawer/AAA_MainActivity.java
@@ -1,6 +1,7 @@
 package com.htkshkoder.navigationdrawer;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Calendar;
@@ -29,24 +31,22 @@ import java.util.Locale;
 public class AAA_MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ImageView imgLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imgLogo = (ImageView)findViewById(R.id.imgLogo);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), secondScreen.class);
+                //exchange Data between views key->value
                 i.putExtra("date","18.05.2017");
                 startActivity(i);
             }
@@ -60,6 +60,7 @@ public class AAA_MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
